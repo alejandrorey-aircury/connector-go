d4be651facd695711a6ce12a5af3593671b6a827
Alejandro Rey <alejandro.rey@aircury.com>
2025-04-06 13:13:44 +0200

    Connector | Definition | Return pointer in processing for better error handling

--- internal/definition/definition.go
+++ internal/definition/definition.go
@@ -20,15 +20,15 @@ func parseDefinition(configurationFilePath string) (Definition, error) {
 	return definition, err
 }
 
-func ProcessDefinition(configurationFilePath string) (Definition, error) {
+func ProcessDefinition(configurationFilePath string) (*Definition, error) {
 	definition, err := parseDefinition(configurationFilePath)
 
 	if err != nil {
-		return Definition{}, &ProcessError{Message: err.Error()}
+		return nil, &ProcessError{Message: err.Error()}
 	}
 
 	definition.Source.URL = os.ExpandEnv(definition.Source.URL)
 	definition.Target.URL = os.ExpandEnv(definition.Target.URL)
 
-	return definition, nil
+	return &definition, nil
 }
