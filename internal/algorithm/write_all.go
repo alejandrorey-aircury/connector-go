5e531a0aeb28b5d4b47a864cd981ae61e85659d5
Alejandro Rey <alejandro.rey@aircury.com>
2025-04-06 19:26:46 +0200

    Connector | Algorithm | Implement new write all algorithm

---
+++ internal/algorithm/write_all.go
@@ -0,0 +1,38 @@
+package algorithm
+
+import (
+	"github.com/aircury/connector/internal/endpoint"
+)
+
+type WriteAllAlgorithm struct {
+	baseAlgorithm
+}
+
+func NewWriteAllAlgorithm(source, target endpoint.Endpoint) *WriteAllAlgorithm {
+	return &WriteAllAlgorithm{
+		baseAlgorithm: baseAlgorithm{
+			Name:   "WriteAll",
+			Source: source,
+			Target: target,
+		},
+	}
+}
+
+func (algorithm *WriteAllAlgorithm) Run() (*DiffOutput, error) {
+	sourceRecords, targetRecords, err := algorithm.FetchData()
+	if err != nil {
+		return nil, err
+	}
+
+	diffOutput := algorithm.CreateDiffOutput(sourceRecords, targetRecords)
+
+	for _, sourceRecord := range sourceRecords {
+		diffOutput.ToInsert = append(diffOutput.ToInsert, sourceRecord)
+	}
+
+	for _, targetRecord := range targetRecords {
+		diffOutput.ToDelete = append(diffOutput.ToDelete, targetRecord)
+	}
+
+	return diffOutput, nil
+}
--- internal/planner/planner.go
+++ internal/planner/planner.go
@@ -11,5 +11,15 @@ type ConnectorPlanner struct {
 }
 
 func (planner *ConnectorPlanner) FindBestAlgorithm() (algorithm.Algorithm, error) {
+	targetCount, err := planner.Target.GetTotalCount()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if targetCount == 0 {
+		return algorithm.NewWriteAllAlgorithm(planner.Source, planner.Target), nil
+	}
+
 	return algorithm.NewSequentialOrderedAlgorithm(planner.Source, planner.Target), nil
 }
